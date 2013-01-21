d5d36a6f606bc8bb535b40afb85580e280cb2dfe
trevor <trevor.stedman@gmail.com>
2013-01-21 00:27:28 +0000

    refactor - pulled out three new classes, ClientSockets, ServerSockets and ServerChannels

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ var
   io = require('socket.io').listen(server),
   store = require('./src/store'),
   serverSockets = require('./src/server-sockets'),
-  clientSockets = require('./src/client-sockets');
+  clientSockets = require('./src/client-sockets'),
+  serverChannels = require('./src/server-channels').init(io);
 
 // use port 8080 - this is what my hosting provider (Modulus) requires.
 server.listen(8080);
@@ -28,47 +29,27 @@ app.get('/', function(req, res) {
 app.get('/connect/:id', function(req, res) {
   var server = store.get(req.params.id);
   if (!!server) {
-    server.get('qrmote_serverinfo', function(err, serverInfo) {
-      res.redirect(serverInfo.remoteUrl + '?_qrmk=' + req.params.id);
-    });
+    res.redirect(server.remoteUrl + '?_qrmk=' + req.params.id);
   }
   else {
     res.send(404);
   }
 });
 
-
 io.of('/initserver').on('connection', function(socket) {
-  
   console.log('initserver socket connection initialised');
   
-  socket.on('qmote_getchannel', function(channel, respond) {
-    console.log('getting channel: ' + channel);
-    new ServerChannel(channel);
+  socket.on('qmote_getchannel', function(channelIndex, respond) {
+    console.log('getting channel: ' + channelIndex);
+
+    var channel = serverChannels.get(channelIndex);
+    channel.on('connection', function(socket) {
+      serverSockets.init(socket, channelIndex);
+    });
+
     respond();
   });
 });
-
-var channels = {};
-
-function ServerChannel(i) {
-  
-  console.log('getting channel: ' + i);
-  
-  var channel = channels[i];
-  if(!!channel) {
-    console.log('got channel: ' + i);
-    return channel;
-  }
-  
-  console.log('creating channel: ' + i);
-  
-  var channel = channels[i] = io.of('/server' + i);
-  channel.on('connection', function(socket) {
-    serverSockets.init(socket, i);
-  });
-  return channel;
-}
 
 io.of('/client').on('connection', function(socket) {
   clientSockets.init(socket);
--- server/src/store.js
+++ server/src/store.js
@@ -1,31 +1,28 @@
-module.exports = (function() {
-  
-  var
-    nextKey = 1,
-    store = {},
+var store = {};
 
-    randomString = function(length, chars) {
-      var result = '';
-      for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-      return result;
-    },
-  
-    nextKey = function() {
-      var key = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-      return store[key] ? nextKey() : key;
-    };
-  
-  return {
-    
-    add: function(object) {
-      var key = nextKey();
-      store[key] = object;
-      return key;
-    },
+function randomString(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+  return result;
+}
 
-    get: function(key) {
-      return store[key];
-    }
-  };
+function nextKey() {
+  var key = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+  return store[key] ? nextKey() : key;
+}
+  
+exports.add = function(object) {
+  var key = nextKey();
+  store[key] = object;
+  return key;
+};
 
-})();
+exports.get = function(key) {
+  return store[key];
+};
+
+exports.remove = function(key) {
+  var object = store[key];
+  delete store[key];
+  return object;
+};
